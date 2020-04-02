6eb6afd96cfaa42e8055ca995daf7ffd1c3790d0
Blademaster680 <dylanprimic@gmail.com>
2020-04-02 14:00:33 +0200

    Fixed the progression bar
    - The calculation fore the progression bar was off by a few percent today. I used datetime.timestamp to get the seconds of the dates to improve the algorythm.

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -5,24 +5,21 @@ from apps.lockdown.models import Country
 
 def indexview(request):
 
-    startDate = to_integer(Country.objects.get(id=1).lockdownStart)
-    endDate = to_integer(Country.objects.get(id=1).lockdownEnd)
-    currentDate = to_integer(datetime.today())
+    startDate = Country.objects.get(id=1).lockdownStart.timestamp()
+    endDate = Country.objects.get(id=1).lockdownEnd.timestamp()
+    currentDate = datetime.today().timestamp()
 
     totalTime = endDate - startDate
     timeLeft = endDate - currentDate
     progression = timeLeft / totalTime * 100
     progression = 100 - progression
 
-    totalDays = totalTime / 1440
+    totalDays = totalTime / 86400
 
     context = {
         'country': Country.objects.get(id=1),
         'progression': round(progression),
-        'totalDays': round(totalDays)
+        'totalDays': round(totalDays),
     }
 
     return render(request, 'frontend/index-video.html', context)
-
-def to_integer(dt_time):
-    return 525600*dt_time.year + 43800*dt_time.month + 1440*dt_time.day
